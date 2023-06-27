1431771bba456507cd2f504d975a355dec2b793a
John Guibas <john@succinct.xyz>
2023-06-26 17:55:59 -0700

    decompose varint

--- src/validator.rs
+++ src/validator.rs
@@ -13,8 +13,11 @@ use plonky2_field::extension::Extendable;
 /// encoded using protobuf's default integer encoding, which consist of 7 bit payloads. You can
 /// read more about them here: https://protobuf.dev/programming-guides/encoding/#varints.
 
-/// The maximum length of a protobuf-encoded Tendermint validator.
+/// The maximum length of a protobuf-encoded Tendermint validator in bytes.
 const VALIDATOR_BYTES_LEN_MAX: usize = 46;
+
+/// The maximum length of a protobuf-encoded Tendermint validator in bits.
+const VALIDATOR_BITS_LEN_MAX: usize = VALIDATOR_BYTES_LEN_MAX * 8;
 
 // The number of bytes in a Tendermint validator's public key.
 const PUBKEY_BYTES_LEN: usize = 32;
@@ -22,9 +25,12 @@ const PUBKEY_BYTES_LEN: usize = 32;
 // The maximum number of bytes in a Tendermint validator's voting power.
 const VOTING_POWER_BYTES_LEN_MAX: usize = 9;
 
+// The maximum number of bits in a Tendermint validator's voting power.
+const VOTING_POWER_BITS_LEN_MAX: usize = VOTING_POWER_BYTES_LEN_MAX * 8;
+
 /// The Ed25519 public key as a list of 32 byte targets.
 #[derive(Debug, Clone, Copy)]
-pub struct Ed25519PubkeyTarget(pub [U8Target; 32]);
+pub struct Ed25519PubkeyTarget(pub [BoolTarget; 256]);
 
 /// The voting power as a list of 2 u32 targets.
 #[derive(Debug, Clone, Copy)]
@@ -37,45 +43,27 @@ struct TendermintValidator {
     pub voting_power: I64Target,
 }
 
-pub trait TendermintVerifier {
+pub trait TendermintBuilder {
+    fn marshal_int64_varint(&mut self, num: I64Target) -> [BoolTarget; VOTING_POWER_BITS_LEN_MAX];
+
     /// Serializes the validator public key and voting power to bytes.
     fn marshal_tendermint_validator(
         &mut self,
         pubkey: Ed25519PubkeyTarget,
         voting_power: I64Target,
-    ) -> [U8Target; VALIDATOR_BYTES_LEN_MAX];
+    ) -> [BoolTarget; VALIDATOR_BITS_LEN_MAX];
 }
 
-impl<F: RichField + Extendable<D>, const D: usize> TendermintVerifier for CircuitBuilder<F, D> {
-    fn marshal_tendermint_validator(
+impl<F: RichField + Extendable<D>, const D: usize> TendermintBuilder for CircuitBuilder<F, D> {
+    fn marshal_int64_varint(
         &mut self,
-        pubkey: Ed25519PubkeyTarget,
         voting_power: I64Target,
-    ) -> [U8Target; VALIDATOR_BYTES_LEN_MAX] {
-        let mut ptr = 0;
+    ) -> [BoolTarget; VOTING_POWER_BITS_LEN_MAX] {
         let zero = self.zero();
         let one = self.one();
-        let zero_u8 = self.zero_u8();
-        let mut serialized_bytes = [zero_u8; VALIDATOR_BYTES_LEN_MAX];
-
-        // The first four prefix bytes of the serialized validator are `10 34 10 32`.
-        let prefix_pubkey_bytes = [10, 34, 10, 32].map(|x| self.constant(F::from_canonical_u64(x)));
-        for i in 0..prefix_pubkey_bytes.len() {
-            serialized_bytes[ptr] = self.new_u8(prefix_pubkey_bytes[i]);
-            ptr += 1;
-        }
-
-        // The next 32 bytes of the serialized validator are the public key.
-        for i in 0..PUBKEY_BYTES_LEN {
-            serialized_bytes[ptr] = pubkey.0[i];
-            ptr += 1;
-        }
-
-        // The next byte of the serialized validator is `16`.
-        let prefix_voting_power_byte = self.constant(F::from_canonical_u64(16));
-        serialized_bytes[ptr] = self.new_u8(prefix_voting_power_byte);
 
         // The remaining bytes of the serialized validator are the voting power as a "varint".
+        // Note: need to be careful regarding U64 and I64 differences.
         let voting_power_bits_lower = self.u32_to_bits_le(voting_power.0[0]);
         let voting_power_bits_upper = self.u32_to_bits_le(voting_power.0[1]);
         let voting_power_bits = [voting_power_bits_lower, voting_power_bits_upper].concat();
@@ -112,6 +100,7 @@ impl<F: RichField + Extendable<D>, const D: usize> TendermintVerifier for Circui
         // If the index of a septet is elss than the last non-zero septet, set the most significant
         // bit of the byte to 1 and copy the septet bits into the lower 7 bits. Otherwise, still
         // copy the bit but the set the most significant bit to zero.
+        let mut buffer = [self._false(); VOTING_POWER_BYTES_LEN_MAX * 8];
         for i in 0..VOTING_POWER_BYTES_LEN_MAX {
             // If the index is less than the last non-zero septet index, `diff` will be in
             // [0, VOTING_POWER_BYTES_LEN_MAX).
@@ -127,14 +116,62 @@ impl<F: RichField + Extendable<D>, const D: usize> TendermintVerifier for Circui
                     self.or(is_lt_last_non_zero_septet_idx, is_candidate);
             }
 
-            // If the index is less than the last non-zero septet index, set the most significant
-            // bit of the byte to 1 and copy the septet bits into the lower 7 bits. Otherwise,
-            // still copy the bit but the set the most significant bit to zero.
-            let base = F::TWO.exp_u64(7);
-            let byte = self.mul_const_add(base, is_lt_last_non_zero_septet_idx.target, septets[i]);
-            serialized_bytes[ptr] = self.new_u8(byte);
+            // Copy septet bits into the buffer.
+            for j in 0..7 {
+                let bit = voting_power_bits[i * 7 + j];
+                buffer[i * 8 + j] = bit;
+            }
+
+            // Set the most significant bit of the byte to 1 if the index is less than the last
+            // non-zero septet index.
+            buffer[i * 8 + 7] = is_lt_last_non_zero_septet_idx;
+        }
+        return buffer;
+    }
+
+    fn marshal_tendermint_validator(
+        &mut self,
+        pubkey: Ed25519PubkeyTarget,
+        voting_power: I64Target,
+    ) -> [BoolTarget; VALIDATOR_BYTES_LEN_MAX * 8] {
+        let mut ptr = 0;
+        let mut buffer = [self._false(); VALIDATOR_BYTES_LEN_MAX * 8];
+
+        // The first four prefix bytes of the serialized validator are `10 34 10 32`.
+        let prefix_pubkey_bytes = [10, 34, 10, 32];
+        for i in 0..prefix_pubkey_bytes.len() {
+            for j in 0..8 {
+                let bit = self.constant(F::from_canonical_u64((prefix_pubkey_bytes[i] >> j) & 1));
+                buffer[ptr] = BoolTarget::new_unsafe(bit);
+                ptr += 1;
+            }
         }
 
-        return serialized_bytes;
+        // The next 32 bytes of the serialized validator are the public key.
+        for i in 0..PUBKEY_BYTES_LEN {
+            for j in 0..8 {
+                buffer[ptr] = pubkey.0[i];
+                ptr += 1;
+            }
+        }
+
+        // The next byte of the serialized validator is `16`.
+        let prefix_voting_power_byte = 16;
+        for j in 0..8 {
+            let bit = self.constant(F::from_canonical_u64((prefix_voting_power_byte >> j) & 1));
+            buffer[ptr] = BoolTarget::new_unsafe(bit);
+            ptr += 1;
+        }
+
+        // The remaining bytes of the serialized validator are the voting power as a "varint".
+        let voting_power_bits = self.marshal_int64_varint(voting_power);
+        for i in 0..VOTING_POWER_BYTES_LEN_MAX {
+            for j in 0..8 {
+                buffer[ptr] = voting_power_bits[i * 8 + j];
+                ptr += 1;
+            }
+        }
+
+        buffer
     }
 }
