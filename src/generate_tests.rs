842adecef0b406e55a160709cff25bfe3734fa3c
Ratan Kaliani <ratankaliani@berkeley.edu>
2023-07-21 15:25:51 -0700

    fix: script hex string

--- src/generate_tests.rs
+++ src/generate_tests.rs
@@ -1,4 +1,4 @@
-use crate::merkle::hash_all_leaves;
+use crate::merkle::{hash_all_leaves, simple_hash_from_byte_vectors};
 use rand::Rng;
 use sha2::Sha256;
 use subtle_encoding::hex;
@@ -9,7 +9,7 @@ pub fn generate_tendermint_test_cases(n: usize) {
     // Generate a random number between 38 and 47
     let mut rng = rand::thread_rng();
 
-    let length = 10;
+    let length = n;
 
     let random_bytes: Vec<Vec<u8>> = (0..length)
         .map(|_| {
@@ -18,21 +18,21 @@ pub fn generate_tendermint_test_cases(n: usize) {
         })
         .collect();
 
-    // Use hash_all_leaves to generate the root hash
-    let root_hash = hash_all_leaves::<Sha256>(&random_bytes);
+    // Use simple_hash_from_byte_vectors to generate the root hash
+    let root_hash = simple_hash_from_byte_vectors::<Sha256>(&random_bytes);
 
     // Print the random byte arrays as an array of hex strings, that have double quotes around them and are separated by commas
 
     let mut hex_strings = Vec::new();
 
     for b in &random_bytes {
-        let hex_string = hex::encode(b);
+        let hex_string = String::from_utf8(hex::encode(b)).expect("Found invalid UTF-8");
         hex_strings.push(hex_string);
     }
 
     // Format the hex strings with double quotes and commas
-    println!("{:?}", hex_strings);
+    println!("Validators: {:?}", hex_strings);
 
     // Print the root hash
-    println!("{:?}", root_hash);
+    println!("Root Hash: {:?}", String::from_utf8(hex::encode(root_hash)).expect("Found invalid UTF-8"));
 }
