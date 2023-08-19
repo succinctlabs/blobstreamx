ce80e614c098b78086e271d2e91a2591c294b7d0
ratankaliani <ratan@succinct.xyz>
2023-08-18 17:40:15 -0700

    wip: using curta import fix, eddsa fix

--- src/step.rs
+++ src/step.rs
@@ -6,7 +6,7 @@
 //! The `pubkey` is encoded as the raw list of bytes used in the public key. The `varint` is
 //! encoded using protobuf's default integer encoding, which consist of 7 bit payloads. You can
 //! read more about them here: https://protobuf.dev/programming-guides/encoding/#varints.
-use curta::plonky2::field::CubicParameters;
+use curta::math::extension::CubicParameters;
 use plonky2::field::extension::Extendable;
 use plonky2::iop::target::BoolTarget;
 use plonky2::plonk::config::AlgebraicHasher;
