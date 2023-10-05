96fa900d2b6662b342ca2e2dcac9cd7073aa9e03
Ratan Kaliani <ratankaliani@berkeley.edu>
2023-10-05 09:49:48 -0700

    fix: bin comments

--- circuits/src/step.rs
+++ circuits/src/step.rs
@@ -1,19 +1,3 @@
-//! To build the binary:
-//!
-//!     `cargo build --release --bin step`
-//!
-//! To build the circuit:
-//!
-//!     `./target/release/circuit_function_field build`
-//!
-//! To prove the circuit using evm io:
-//!
-//!    `./target/release/circuit_function_evm prove --input-json src/bin/circuit_function_evm_input.json`
-//!
-//! Note that this circuit will not work with field-based io.
-//!
-//!
-//!
 use plonky2x::backend::circuit::Circuit;
 use plonky2x::frontend::hint::simple::hint::Hint;
 use plonky2x::frontend::uint::uint64::U64Variable;
