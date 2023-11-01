2f6e5a6c5337c313bc5c139cbfe599cbc065fb2d
Ratan Kaliani <ratankaliani@berkeley.edu>
2023-11-01 11:42:56 -0700

    clean up blobstreamx script

--- bin/blobstreamx.rs
+++ bin/blobstreamx.rs
@@ -1,13 +1,12 @@
 use std::env;
-use std::sync::Arc;
 
+use alloy_sol_types::{sol, SolType};
 use ethers::contract::abigen;
 use ethers::core::types::Address;
-use ethers::prelude::SignerMiddleware;
 use ethers::providers::{Http, Provider};
-use ethers::signers::{LocalWallet, Signer};
-use ethers::types::H256;
+use ethers::types::{Bytes, H256};
 use log::info;
+use subtle_encoding::hex;
 use tendermint::block::Header;
 use tendermintx::input::tendermint_utils::{
     is_valid_skip, HeaderResponse, SignedBlock, SignedBlockResponse,
@@ -33,13 +32,135 @@ struct OffchainInput {
     input: String,
 }
 
-type NextHeaderCalldataTuple = sol! { tuple(uint64) };
+type NextHeaderCalldataTuple = sol! { tuple(uint64,) };
 type NextHeaderInputTuple = sol! { tuple(uint64, bytes32) };
 
 type HeaderRangeCalldataTuple = sol! { tuple(uint64, uint64) };
 type HeaderRangeInputTuple = sol! { tuple(uint64, bytes32, uint64) };
 
-async fn get_latest_header(base_url: &str) -> Header {
+fn get_config() -> BlobstreamConfig {
+    // TODO: Update function id's
+    let next_header_function_id = H256::from_slice(
+        &hex::decode("98a2381f5efeaf7c3e39d749d6f676df1432487578f393161cebd2b03934f43b").unwrap(),
+    );
+    let header_range_function_id = H256::from_slice(
+        &hex::decode("b3f1415062a3543bb1c48d9d6a49f9e005fe415d347a5ba63e40bb1235acfd86").unwrap(),
+    );
+    let contract_address = env::var("CONTRACT_ADDRESS").expect("CONTRACT_ADDRESS must be set");
+
+    // TODO: BlobstreamX on Goerli: https://goerli.etherscan.io/address/#code
+    let address = contract_address
+        .parse::<Address>()
+        .expect("invalid address");
+
+    BlobstreamConfig {
+        address,
+        chain_id: 5,
+        next_header_function_id,
+        header_range_function_id,
+    }
+}
+
+async fn submit_request(
+    config: &BlobstreamConfig,
+    function_data: Vec<u8>,
+    input: Vec<u8>,
+    function_id: H256,
+) {
+    // All data except for chainId is a string, and needs a 0x prefix.
+    let data = OffchainInput {
+        chainId: config.chain_id,
+        to: Bytes::from(config.address.0).to_string(),
+        data: Bytes::from(function_data).to_string(),
+        functionId: Bytes::from(function_id.0).to_string(),
+        input: Bytes::from(input).to_string(),
+    };
+
+    // Stringify the data into JSON format.
+    let serialized_data = serde_json::to_string(&data).unwrap();
+
+    // TODO: Update with config.
+    let request_url = "https://alpha.succinct.xyz/api/request/new";
+
+    // Submit POST request to the offchain worker.
+    let client = reqwest::Client::new();
+    let res = client
+        .post(request_url)
+        .header("Content-Type", "application/json")
+        .body(serialized_data)
+        .send()
+        .await
+        .expect("Failed to send request.");
+
+    if res.status().is_success() {
+        // TODO: Log success message. Find structure of output.
+        info!("Successfully submitted request.");
+    } else {
+        // TODO: Log error message.
+        info!("Failed to submit request.");
+    }
+}
+
+async fn request_next_header(
+    config: &BlobstreamConfig,
+    contract: &BlobstreamX<Provider<Http>>,
+    trusted_block: u64,
+) {
+    let trusted_header_hash = contract
+        .block_height_to_header_hash(trusted_block)
+        .await
+        .unwrap();
+
+    let input = NextHeaderInputTuple::abi_encode_packed(&(trusted_block, trusted_header_hash));
+
+    info!("length of step input: {:?}", input.len());
+
+    let function_signature = "commitNextHeader(uint64)";
+    let function_selector = ethers::utils::id(function_signature).to_vec();
+    let encoded_parameters = NextHeaderCalldataTuple::abi_encode_sequence(&(trusted_block,));
+    // Concat function selector and encoded parameters.
+    let function_data = [&function_selector[..], &encoded_parameters[..]].concat();
+
+    submit_request(config, function_data, input, config.next_header_function_id).await;
+}
+
+async fn request_header_range(
+    config: &BlobstreamConfig,
+    contract: &BlobstreamX<Provider<Http>>,
+    trusted_block: u64,
+    target_block: u64,
+) {
+    let trusted_header_hash = contract
+        .block_height_to_header_hash(trusted_block)
+        .await
+        .unwrap();
+
+    let input = HeaderRangeInputTuple::abi_encode_packed(&(
+        trusted_block,
+        trusted_header_hash,
+        target_block,
+    ));
+
+    info!("length of rotate input: {:?}", input.len());
+
+    let function_signature = "commitHeaderRange(uint64,uint64)";
+    let function_selector = ethers::utils::id(function_signature).to_vec();
+    let encoded_parameters =
+        HeaderRangeCalldataTuple::abi_encode_sequence(&(trusted_block, target_block));
+    // Concat function selector and encoded parameters.
+    let function_data = [&function_selector[..], &encoded_parameters[..]].concat();
+
+    submit_request(
+        config,
+        function_data,
+        input,
+        config.header_range_function_id,
+    )
+    .await;
+}
+
+// Tendermint RPC Fetcher
+async fn get_latest_tendermint_header(base_url: &str) -> Header {
     let query_url = format!("{}/header", base_url);
     info!("Querying url {:?}", query_url.as_str());
     let res = reqwest::get(query_url).await.unwrap().text().await.unwrap();
@@ -47,15 +168,7 @@ async fn get_latest_header(base_url: &str) -> Header {
     v.result.header
 }
 
-async fn get_header_from_number(base_url: &str, block_number: u64) -> Header {
-    let query_url = format!("{}/header?height={}", base_url, block_number);
-    info!("Querying url {:?}", query_url.as_str());
-    let res = reqwest::get(query_url).await.unwrap().text().await.unwrap();
-    let v: HeaderResponse = serde_json::from_str(&res).expect("Failed to parse JSON");
-    v.result.header
-}
-
-async fn get_signed_block_from_number(base_url: &str, block_number: u64) -> SignedBlock {
+async fn get_tendermint_signed_block_from_number(base_url: &str, block_number: u64) -> SignedBlock {
     let query_url = format!("{}/signed_block?height={}", base_url, block_number);
     info!("Querying url {:?}", query_url.as_str());
     let res = reqwest::get(query_url).await.unwrap().text().await.unwrap();
@@ -66,7 +179,7 @@ async fn get_signed_block_from_number(base_url: &str, block_number: u64) -> Sign
 // Binary search to find the block number to call request_combined_skip on. If the binary search
 // returns start_block + 1, then we call request_combined_step instead.
 async fn find_request_block(base_url: &str, start_block: u64, end_block: u64) -> u64 {
-    let start_signed_block = get_signed_block_from_number(base_url, start_block).await;
+    let start_signed_block = get_tendermint_signed_block_from_number(base_url, start_block).await;
 
     let mut curr_end_block = end_block;
     loop {
@@ -74,7 +187,8 @@ async fn find_request_block(base_url: &str, start_block: u64, end_block: u64) ->
             return curr_end_block;
         }
 
-        let curr_end_signed_block = get_signed_block_from_number(base_url, curr_end_block).await;
+        let curr_end_signed_block =
+            get_tendermint_signed_block_from_number(base_url, curr_end_block).await;
 
         if is_valid_skip(&start_signed_block, &curr_end_signed_block) {
             return curr_end_block;
@@ -89,89 +203,43 @@ async fn find_request_block(base_url: &str, start_block: u64, end_block: u64) ->
 async fn main() {
     dotenv::dotenv().ok();
 
-    let tendermint_rpc_url = env::var("RPC_MOCHA_4").expect("RPC_MOCHA_4 must be set");
+    // Loop every 30 minutes.
+    const LOOP_DELAY: u64 = 30;
+
+    let config: BlobstreamConfig = get_config();
+
+    let tendermint_rpc_url =
+        env::var("TENDERMINT_RPC_URL").expect("TENDERMINT_RPC_URL must be set");
 
     let ethereum_rpc_url = env::var("RPC_URL").expect("RPC_URL must be set");
     let provider =
         Provider::<Http>::try_from(ethereum_rpc_url).expect("could not connect to client");
 
-    let private_key = env::var("PRIVATE_KEY").expect("PRIVATE_KEY must be set");
-    let wallet: LocalWallet = private_key
-        .parse::<LocalWallet>()
-        .expect("invalid private key")
-        .with_chain_id(5u64);
+    let blobstreamx = BlobstreamX::new(config.address, provider.into());
 
-    info!("Wallet address: {:?}", wallet.address());
-
-    let client = SignerMiddleware::new(provider, wallet);
-    let client = Arc::new(client);
-
-    // BlobstreamX on Goerli: https://goerli.etherscan.io/address/0x67ea962864cdad3f2202118dc6f65ff510f7bb4d#code
-    let address = "0x67ea962864cdad3f2202118dc6f65ff510f7bb4d";
-    let address = address.parse::<Address>().expect("invalid address");
-
-    let blobstreamx = BlobstreamX::new(address, client.clone());
-    let latest_header = get_latest_header(&tendermint_rpc_url).await;
-    let latest_block = latest_header.height.value();
-
-    let head = blobstreamx
-        .latest_block()
-        .call()
-        .await
-        .expect("failed to get head");
-
-    // TODO: Remove in prod
-    // Set genesis header if we are more than 1000 blocks behind.
-    if (head as u64) < latest_block - 1000 {
-        let mut block_to_request = latest_block - 500;
-        block_to_request = block_to_request - (block_to_request % 10);
-
-        let genesis_header = get_header_from_number(&tendermint_rpc_url, block_to_request).await;
-        blobstreamx
-            .set_genesis_header(
-                block_to_request,
-                H256::from_slice(genesis_header.hash().as_bytes()).0,
-            )
-            .send()
-            .await
-            .expect("failed to set genesis header");
-    }
-
-    let mut curr_head = head;
-
-    // Loop every 30 minutes. Call request_combined_skip every 30 minutes with the latest block number.
-    // Loop time is currently to the time it takes for a proof to be generated.
-    // TODO: Update with prod loop time.
-    // TODO: Can update this to only skip to multiples of 10/100.
-    let increment = 30;
+    let header_range_max = blobstreamx.data_commitment_max().await.unwrap();
     loop {
-        // Get latest_header
-        let latest_header = get_latest_header(&tendermint_rpc_url).await;
+        let current_block = blobstreamx.latest_block().await.unwrap();
+
+        // Get the head of the chain.
+        let latest_header = get_latest_tendermint_header(&tendermint_rpc_url).await;
         let latest_block = latest_header.height.value();
 
-        // Round down to the nearest 10.
-        let max_end_block = latest_block - 10;
+        // Subtract 2 blocks to account for the time it takes for a block to be processed by
+        // consensus.
+        let max_end_block = std::cmp::min(latest_block - 2, current_block + header_range_max);
 
-        let target_block = find_request_block(&tendermint_rpc_url, curr_head, max_end_block).await;
+        let target_block =
+            find_request_block(&tendermint_rpc_url, current_block, max_end_block).await;
 
-        if target_block - curr_head == 1 {
-            // Call step if necessary.
-            blobstreamx
-                .request_next_header()
-                .send()
-                .await
-                .expect("failed to request combined skip");
+        if target_block - current_block == 1 {
+            // Request the next header if the target block is the next block.
+            request_next_header(&config, &blobstreamx, current_block).await;
         } else {
-            // Verify the call succeeded.
-            blobstreamx
-                .request_header_range(target_block)
-                .send()
-                .await
-                .expect("failed to request combined skip");
+            // Request a header range if the target block is not the next block.
+            request_header_range(&config, &blobstreamx, current_block, target_block).await;
         }
 
-        tokio::time::sleep(tokio::time::Duration::from_secs(60 * increment)).await;
-
-        curr_head = target_block;
+        tokio::time::sleep(tokio::time::Duration::from_secs(60 * LOOP_DELAY)).await;
     }
 }
