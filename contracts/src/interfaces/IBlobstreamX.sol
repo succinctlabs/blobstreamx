33ff4ffd6dcfb876bc99a1de852368a276273fbe
Ratan Kaliani <ratankaliani@berkeley.edu>
2023-11-03 19:10:11 -0700

    feat(contracts): Implement `IDAOracle` (#71)
    * Implement IDAOracle on BlobstreamX
    
    * add source for data commitment max
    
    * feat: Update ci
    
    * update to checkout v3
    
    * update ci action
    
    * try another workflow
    
    * try another workflow
    
    * try new workflow 3
    
    * try new workflow 4
    
    * try 5
    
    * revert back to ssh agent
    
    * update clippy test

--- contracts/src/interfaces/IBlobstreamX.sol
+++ contracts/src/interfaces/IBlobstreamX.sol
@@ -33,6 +33,9 @@ interface IBlobstreamX {
         uint64 indexed targetBlock
     );
 
+    /// @notice Data commitment for specified block range does not exist.
+    error DataCommitmentNotFound();
+
     /// @notice Get the data commitment for a block range [startBlock, endBlock).
     /// @param startBlock The start block of the block range.
     /// @param endBlock The end block of the block range.
@@ -40,16 +43,4 @@ interface IBlobstreamX {
         uint64 startBlock,
         uint64 endBlock
     ) external view returns (bytes32);
-
-    /// @notice Verify a merkle proof for a specific block's data root against a data commitment containing the block.
-    /// @param startBlock The start block of the block range that contains the proof's block.
-    /// @param endBlock The end block of the block range that contains the proof's block.
-    /// @param _tuple The data root tuple which is the leaf node of the proof and contains the block's data root.
-    /// @param _proof The merkle proof to verify against the data commitment.
-    function verifyAttestation(
-        uint256 startBlock,
-        uint256 endBlock,
-        DataRootTuple memory _tuple,
-        BinaryMerkleProof memory _proof
-    ) external view returns (bool);
 }
