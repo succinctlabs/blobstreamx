46ab7409c17744e558649c54bb94424f5ac80a92
Ratan Kaliani <ratankaliani@berkeley.edu>
2023-12-07 17:23:57 -0800

    clean

--- contracts/script/Deploy.s.sol
+++ contracts/script/Deploy.s.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.22;
 
 import "forge-std/Script.sol";
 import {BlobstreamX} from "../src/BlobstreamX.sol";
-import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+import {ERC1967Proxy} from "@openzeppelin/proxy/ERC1967/ERC1967Proxy.sol";
 
 contract DeployScript is Script {
     function setUp() public {}
