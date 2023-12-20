9788c7b80bf420537bd3423a3e14683bc675246b
Ratan Kaliani <ratankaliani@berkeley.edu>
2023-12-19 22:43:33 -0800

    clean

--- contracts/src/BlobstreamX.sol
+++ contracts/src/BlobstreamX.sol
@@ -19,7 +19,7 @@ contract BlobstreamX is
     /// @notice The address of the gateway contract.
     address public gateway;
 
-    /// @notice The block after the latest block that has been committed.
+    /// @notice The block is the first one in the next data commitment.
     uint64 public latestBlock;
 
     /// @notice The maximum number of blocks that can be skipped in a single request.
