26d1df111fdf920e9d4169601eded7c5b64451c8
Ratan Kaliani <ratankaliani@berkeley.edu>
2024-02-26 23:49:21 -0800

    upload release

--- scripts/upload_release.sh
+++ scripts/upload_release.sh
@@ -1,3 +1,5 @@
+set -e
+
 # Read release id from parameters
 RELEASE_ID=$1
 OUTPUT_FOLDER=$2
@@ -13,7 +15,7 @@ echo "OUTPUT_FOLDER: ${OUTPUT_FOLDER}"
 mkdir -p ./${OUTPUT_FOLDER}
 
 # Copy the release from R2 to local
-AWS_PROFILE=r2 aws s3 cp --endpoint-url ${R2_ENDPOINT} s3://platform-artifacts/main/releases/${RELEASE_ID} ./${OUTPUT_FOLDER}
+AWS_PROFILE=r2 aws s3 cp --endpoint-url ${R2_ENDPOINT} s3://platform-artifacts/main/releases/${RELEASE_ID}/ ./${OUTPUT_FOLDER} --recursive
 
 # tar the release folder
 tar -czvf ${RELEASE_ID}.tar.gz ${OUTPUT_FOLDER}
@@ -22,3 +24,4 @@ tar -czvf ${RELEASE_ID}.tar.gz ${OUTPUT_FOLDER}
 AWS_PROFILE=default aws s3 cp ${RELEASE_ID}.tar.gz s3://public-blobstreamx-circuits/${RELEASE_ID}.tar.gz --endpoint-url ${S3_ENDPOINT}
 
 # TODO: Clean up the tar file
+rm -f ${RELEASE_ID}.tar.gz
