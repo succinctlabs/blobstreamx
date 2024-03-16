459fbdc5bbd582afeb3a4c418561d28e88d06194
Ratan Kaliani <ratankaliani@berkeley.edu>
2024-03-15 18:47:45 -0700

    fix

--- bin/header_range.rs
+++ bin/header_range.rs
@@ -21,7 +21,6 @@ use plonky2x::backend::function::Plonky2xFunction;
 use tendermintx::config::MOCHA_4_CHAIN_ID_SIZE_BYTES;
 
 fn main() {
-    // Celestia's maxmimum data commitment size is 1000: https://github.com/celestiaorg/celestia-core/blob/6933af1ead0ddf4a8c7516690e3674c6cdfa7bd8/pkg/consts/consts.go#L44.
     const VALIDATOR_SET_SIZE_MAX: usize = 100;
     CombinedSkipCircuit::<
         VALIDATOR_SET_SIZE_MAX,
