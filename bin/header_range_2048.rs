b1c0106bd5ca14aaa7e55103e393789e29c47e86
Ratan Kaliani <ratankaliani@berkeley.edu>
2024-03-20 01:27:32 -0700

    feat: header range

--- bin/header_range_2048.rs
+++ bin/header_range_2048.rs
@@ -4,7 +4,6 @@ use plonky2x::backend::function::Plonky2xFunction;
 use tendermintx::config::CELESTIA_CHAIN_ID_SIZE_BYTES;
 
 fn main() {
-    // Celestia's maxmimum data commitment size is 1000: https://github.com/celestiaorg/celestia-core/blob/6933af1ead0ddf4a8c7516690e3674c6cdfa7bd8/pkg/consts/consts.go#L44.
     const VALIDATOR_SET_SIZE_MAX: usize = 100;
     const NB_MAP_JOBS: usize = 32;
     const BATCH_SIZE: usize = 64;
