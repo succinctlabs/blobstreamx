0a8aa229834f14e590649748abb0ee2f7074e0ee
Ratan Kaliani <ratankaliani@berkeley.edu>
2024-04-08 10:58:51 -0700

    update script

--- bin/genesis.rs
+++ bin/genesis.rs
@@ -16,8 +16,8 @@ use tendermintx::input::InputDataFetcher;
 #[derive(Parser, Debug, Clone)]
 #[command(about = "Get the genesis parameters from a block.")]
 pub struct GenesisArgs {
-    #[arg(long, default_value = "1")]
-    pub block: u64,
+    #[arg(long)]
+    pub block: Option<u64>,
 }
 
 #[tokio::main]
@@ -25,14 +25,24 @@ pub async fn main() {
     env::set_var("RUST_LOG", "info");
     dotenv::dotenv().ok();
     env_logger::init();
-    let data_fetcher = InputDataFetcher::default();
+    let mut data_fetcher = InputDataFetcher::default();
     let args = GenesisArgs::parse();
 
-    let genesis_block = args.block;
-
-    let signed_header = data_fetcher
-        .get_signed_header_from_number(genesis_block)
-        .await;
-    let header_hash = signed_header.header.hash();
-    info!("Block {}'s header hash: {:?}", genesis_block, header_hash);
+    if let Some(block) = args.block {
+        let signed_header = data_fetcher.get_signed_header_from_number(block).await;
+        let header_hash = signed_header.header.hash();
+        info!(
+            "GENESIS_HEIGHT: {:?}\n GENESIS_HEADER: {}",
+            block,
+            format!("0x{}", header_hash.to_string())
+        );
+    } else {
+        let signed_header = data_fetcher.get_latest_signed_header().await;
+        let header_hash = signed_header.header.hash();
+        info!(
+            "\nGENESIS_HEIGHT={:?}\nGENESIS_HEADER={}",
+            signed_header.header.height.value(),
+            format!("0x{}", header_hash.to_string())
+        );
+    }
 }
